8c2d52c217c6c590440a48d9e6af72296a8dc5fe
g3n1k <g3n1k@yahoo.com>
2016-09-16 06:33:26 +0700

    docker-compose & hello-world test

--- get-docker.sh
+++ get-docker.sh
@@ -1,2 +1,10 @@
+#!/bin/bash
 wget -qO- https://get.docker.com/ | sh
-sudo usermod -aG docker ub
+sudo usermod -aG docker $(whoami)
+sudo apt-get -y install python-pip
+sudo pip install docker-compose
+mkdir ~/hello-world && cd $_
+touch docker-compose.yml
+echo "my-test:" >> docker-compose.yml
+echo "  image: hello-world" >> docker-compose.yml
+docker-compose up
